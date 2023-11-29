877e471bae9dee466f743c3af73cbad03beb0fa9
Rubi <simrantamang6008@gmail.com>
2023-11-29 16:55:34 +0530

    designed cards and fonts a/c to figma

--- main.js
+++ main.js
@@ -1,24 +1,7 @@
 let container = document.querySelector(".container");
-let alphabet;       //This variable is used for to create A to Z capital alphabets.
+let alphabet;       //This variable is used to create A to Z capital alphabets.
 
-// Inside the below array there are images from A to Z, which we will put on the back side of the card with the help of a loop.
-const backgroundImage = [
-  "backgroundImages/a.png","backgroundImages/b.png",
-  "backgroundImages/c.png","backgroundImages/d.png",
-  "backgroundImages/e.png","backgroundImages/f.png",
-  "backgroundImages/g.png","backgroundImages/h.png",
-  "backgroundImages/i.png","backgroundImages/j.png",
-  "backgroundImages/k.png","backgroundImages/l.png",
-  "backgroundImages/m.png","backgroundImages/n.png",
-  "backgroundImages/o.png","backgroundImages/p.png",
-  "backgroundImages/q.png","backgroundImages/r.png",
-  "backgroundImages/s.png","backgroundImages/t.png",
-  "backgroundImages/u.png","backgroundImages/v.png",
-  "backgroundImages/w.png","backgroundImages/x.png",
-  "backgroundImages/y.png","backgroundImages/z.png",
-];
-
-// We have made this array for the background design of the front cards, in which we will put 6 different designs in the background of the front card with the help of loop.
+//this array for the background design of the front cards, in which we will put 6 different designs in the background of the front card with the help of loop.
 let design = ["design1", "design2", "design3", "design4", "design5", "design6"];
 
 //In this loop the i is running 65 to 90, which is creating 26 divs and j is working for the design array. 
@@ -28,34 +11,34 @@ for (let i = 65, j = 0; i < 91; i++, j++) {
 
   alphabet = String.fromCharCode(i);
 
-  let cell = document.createElement("div");         //This creates front face of card
-  let backCell = document.createElement("div");     // This creates back face of card
-  let containerTwo = document.createElement("div"); //This container will contain both front and back card
+  //creating divs 
+  let frontCard = document.createElement("div");         
+  let backCard = document.createElement("div");     
+  let cardsContainer = document.createElement("div"); 
   let img = document.createElement("img");
   let text = document.createElement("p");
 
-
-  //Now we will give class to div or tag we have created 
-  containerTwo.setAttribute("class", "containerTwo");
-  backCell.setAttribute("class", "backCell");
-  cell.setAttribute("class", "cell");
-  containerTwo.setAttribute("class", "containerTwo");
+  //give class to div or tag we have created 
+  cardsContainer.setAttribute("class", "cardsContainer");
+  backCard.setAttribute("class", "backCard");
+  frontCard.setAttribute("class", "frontCard");
+  cardsContainer.setAttribute("class", "cardsContainer");
   img.setAttribute("class", "img");
   text.setAttribute("class", "text");
-  cell.classList.add(design[j]);    
+  frontCard.classList.add(design[j]);    
 
-  //Now we will append all the divs and tags with the ones we have created for them.
-  container.appendChild(containerTwo);
-  containerTwo.appendChild(cell);
-  containerTwo.appendChild(backCell);
-  backCell.appendChild(img);
-  cell.appendChild(text);
+  //append all the divs and elements with the ones we have created for them.
+  container.appendChild(cardsContainer);
+  cardsContainer.appendChild(frontCard);
+  cardsContainer.appendChild(backCard);
+  backCard.appendChild(img);
+  frontCard.appendChild(text);
   text.innerText = alphabet;
 
-  img.setAttribute("src", backgroundImage[i - 65]); //Now we have to set image in back face of card, we write [i - 65] because in above loop we started i from 65 but our image position is starting from 0. So for changing i value 65 to 0 we write backgroundImage[i - 65]
+  img.setAttribute("src", `backCardImages/${alphabet.toLowerCase()}.png`); //Now we have to set image in back face of card, we write [i - 65] because in above loop we started i from 65 but our image position is starting from 0. So for changing i value 65 to 0 we write backgroundImage[i - 65]
 
-  // We have created a click eventListener for card flipping on click.
-  containerTwo.addEventListener("click", () => {   
-    containerTwo.classList.toggle("flipped");
+  // Flip on click
+  cardsContainer.addEventListener("click", () => {   
+    cardsContainer.classList.toggle("flipped");
   });
 }
