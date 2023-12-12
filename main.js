086a3b91014c2ce37c1f31776314749e0b3eb2e3
Rubi <simrantamang6008@gmail.com>
2023-12-12 16:14:32 +0530

    make changes heart icon a/c to figma

--- main.js
+++ main.js
@@ -3,29 +3,29 @@ let carouselImg = document.querySelector(".carouselImg");
 let dots = document.querySelectorAll(".dot");
 
 let i = 0;
-let alphabet;       //This variable is used to create A to Z capital alphabets.
+let alphabet; //This variable is used to create A to Z capital alphabets.
 
 const crslImg = [
   "assets/carouselImages/IMAGE.png",
   "assets/carouselImages/img2.jpeg",
-  "assets/carouselImages/img4.jpeg"
-]
+  "assets/carouselImages/img4.jpeg",
+];
 const wordPro = [
-  "apple", "ball", "cat", "dog", "elephant", "fish",
-  "goat", "horse", "igloo", "joker", "kite", "lion",
-  "monkey", "nose", "octopus", "pig", "queen",
-  "rocket", "snake", "tiger", "umbrella", "van",
-  "watermellon", "x-ray", "yak", "zebra"
-]
+  "apple","ball","cat","dog","elephant",
+  "fish","goat","horse","igloo","joker",
+  "kite","lion","monkey","nose","octopus",
+  "pig","queen","rocket","snake","tiger",
+  "umbrella","van","watermellon","x-ray",
+  "yak","zebra",
+];
 setInterval(function () {
   i++;
-  let selectedDotId = "#dot"+ i;
-  let selectedDot = document.querySelector(selectedDotId);  
+  let selectedDotId = "#dot" + i;
+  let selectedDot = document.querySelector(selectedDotId);
   // console.log(selectedDot);
-  dots.forEach(dot=>dot.style.backgroundColor = "white")
+  dots.forEach((dot) => (dot.style.backgroundColor = "white"));
   selectedDot.style.backgroundColor = "black";
-  if (i >= 3) 
-    i = 0;
+  if (i >= 3) i = 0;
   carouselImg.setAttribute("src", crslImg[i]);
 }, 2000);
 
@@ -33,7 +33,7 @@ setInterval(function () {
 let design = ["design1", "design2", "design3", "design4", "design5", "design6"];
 
 //In this loop the i is running 65 to 90, which is creating 26 divs and j is working for the design array.
-for (let i = 65, k=0, j = 0; i < 91; k++, i++, j++) {
+for (let i = 65, k = 0, j = 0; i < 91; k++, i++, j++) {
   if (j >= 6)
     //This condition is telling that our j will run till 6, when the value of j exceeds 6 then the value of j will again become 0.
     j = 0;
@@ -47,16 +47,15 @@ for (let i = 65, k=0, j = 0; i < 91; k++, i++, j++) {
   let img = document.createElement("img");
   let text = document.createElement("p");
   let imgText = document.createElement("p");
-  let imgIconOne = document.createElement("img")
-  let imgIconTwo = document.createElement("img")
-  let imgIconThree = document.createElement("img")
+  let imgIconOne = document.createElement("img");
+  let imgIconTwo = document.createElement("img");
+  let imgIconThree = document.createElement("img");
+  let iconContainer = document.createElement("div");
 
-  
-//.........Giving icon src path to img tag.......
-  imgIconOne.src = "assets/backImgIcon/heartIcon.png"
-  imgIconTwo.src = "assets/backImgIcon/sizeIcon.png"
-  imgIconThree.src = "assets/backImgIcon/shareIcon.png"
-
+  //.........Giving icon src path to img tag.......
+  imgIconOne.src = "assets/backImgIcon/heartIcon.png";
+  imgIconTwo.src = "assets/backImgIcon/sizeIcon.png";
+  imgIconThree.src = "assets/backImgIcon/shareIcon.png";
 
   //give class to div or tag we have created
   cardsContainer.setAttribute("class", "cardsContainer");
@@ -66,9 +65,10 @@ for (let i = 65, k=0, j = 0; i < 91; k++, i++, j++) {
   img.setAttribute("class", "img");
   text.setAttribute("class", "text");
   imgText.setAttribute("class", "bckImgText");
-  imgIconOne.setAttribute("id", "imgIconOne")
-  imgIconTwo.setAttribute("id", "imgIconTwo")
-  imgIconThree.setAttribute("id", "imgIconThree")
+  imgIconOne.setAttribute("id", "imgIconOne");
+  imgIconTwo.setAttribute("id", "imgIconTwo");
+  imgIconThree.setAttribute("id", "imgIconThree");
+  iconContainer.setAttribute("class", "iconContainer");
 
   frontCard.classList.add(design[j]);
 
@@ -78,13 +78,13 @@ for (let i = 65, k=0, j = 0; i < 91; k++, i++, j++) {
   cardsContainer.appendChild(backCard);
   backCard.appendChild(img);
   frontCard.appendChild(text);
+  backCard.appendChild(iconContainer);
   backCard.appendChild(imgText);
-  backCard.appendChild(imgIconOne);
-  backCard.appendChild(imgIconTwo)
-  backCard.appendChild(imgIconThree);
-  imgText.innerText = wordPro[k]; 
+  iconContainer.appendChild(imgIconOne);
+  iconContainer.appendChild(imgIconTwo);
+  iconContainer.appendChild(imgIconThree);
+  imgText.innerText = wordPro[k];
   text.innerText = alphabet;
-
 
   img.setAttribute("src", `assets/${alphabet.toLowerCase()}.png`); //set image in back face of card, we write [i - 65] because in above loop we started i from 65 but our image position is starting from 0. So for changing i value 65 to 0 we write backgroundImage[i - 65]
 
@@ -93,11 +93,17 @@ for (let i = 65, k=0, j = 0; i < 91; k++, i++, j++) {
     cardsContainer.classList.toggle("flipped");
     let msg = new SpeechSynthesisUtterance();
     msg.text = wordPro[k];
-    window.speechSynthesis.speak(msg)
+    window.speechSynthesis.speak(msg);
   });
-  let redHeart = 
-  imgIconOne.addEventListener("click", () =>{
-    //imgIconOne.style.backgroundColor = "red"
-    imgIconOne.src = "assets/backImgIcon/heartRed.png"
-  })
+
+  let heartClick = 0;
+  imgIconOne.addEventListener("click", () => {
+    if (heartClick === 0) {
+      imgIconOne.src = "assets/backImgIcon/redHeart.png";
+      heartClick = 1;
+    } else {
+      imgIconOne.src = "assets/backImgIcon/heartIcon.png";
+      heartClick = 0;
+    }
+  });
 }
