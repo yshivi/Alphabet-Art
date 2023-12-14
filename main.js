f0257cc4c699014c9bca31dc990cb5289eca443f
yshivi <yshivi825@gmail.com>
2023-12-14 07:43:18 +0530

    make some change with feedback

---
+++ main.js
@@ -0,0 +1,93 @@
+// Make a container in which all Alphabet 
+const container = document.querySelector(".container")
+
+const carouselImg = document.querySelector(".carousel-img")
+
+// Make a dots in which  all dot 
+const dots = document.querySelectorAll(".dot")
+
+// make a var for store aphabets
+let i=0;
+let alphabet;
+
+
+// Make a pattern array in which store classes for background styling in front-div
+let pattern = ["print1", "print2", "print3", "print4", "print5", "print6"]
+//This "For loop" used to making alphabet divs, and changing background design with alphabet divs, and changeing image
+// i var for make alphabets 
+// j variable for pattern array
+for (let i = 65, j = 0,k=0; i <= 90;  i++, j++,k++) {
+
+    if (j > 5)
+        j = 0
+
+       
+    // Number 65-90 to make alphabet a to z
+    alphabet = String.fromCharCode(i)
+
+     // Make a container in which will be front and back both card and append flipcontainer to container
+    let frontDiv = document.createElement("div")
+    let backDivs = document.createElement("div")
+    container.appendChild(backDivs)
+    container.appendChild(frontDiv)
+    frontDiv.setAttribute("class", "front-div")
+    backDivs.setAttribute("class", "backCard-cotainer")
+   
+    frontDiv.classList.add(pattern[j])
+    let text = document.createElement("p")
+    text.setAttribute("class", "text")
+    frontDiv.appendChild(text)
+    text.innerHTML = alphabet
+
+// Add imges in the back-div of the cards and we added images  
+    let imageArr = document.createElement("img")
+    imageArr.setAttribute("src", `images/alphabetImages/${alphabet.toLocaleLowerCase()}.png`)
+    imageArr.setAttribute("class", "image-class")
+    backDivs.appendChild(imageArr)
+    //console.log(imageArr)
+
+    let flipCard =document.createElement("div")
+    flipCard.appendChild(frontDiv)
+    flipCard.appendChild(backDivs)
+    container.appendChild(flipCard)
+    flipCard.setAttribute("class", "flipCard")
+
+    //When we will click any abphabet then show back side of the card
+
+    flipCard.addEventListener("click",()=>{
+        flipCard.classList.toggle("flipped")
+//this autoplay for carousel images 
+        let soundArr = ["apple", "ball", "cat", "dog", "elephant", "fish", "giraffe", "hourse", "igloo", "joker", "king", "lion"
+            , "monkey", "nose", "octopus", "pen", "queen", "rabbit", "snake", "tiger", "umbrela", "ven", "watermelon", "x-ray", "yak", "zebra"]
+        let sound = new SpeechSynthesisUtterance();
+        sound.text = soundArr[k]
+        window.speechSynthesis.speak(sound);
+    })
+
+
+
+}
+// this  carouselImg array for carousel images
+let carouselImgArr = [
+    "images/crousel image/image1.jpeg",
+    "images/crousel image/image2.jpeg",
+    "images/crousel image/image3.jpeg"
+]
+    // console.log(carouselImg)
+
+autoplay=setInterval(function(){
+    i++;
+    let selectedDotId = "#dot" + i;
+    let selectedDot = document.querySelector(selectedDotId);
+
+    dots.forEach(dot => dot.style.backgroundColor = "white");
+    selectedDot.style.backgroundColor = "black";
+
+    if (i > 2)
+    i = 0;
+carouselImg.setAttribute("src", carouselImgArr[i]);
+}, 1200);
+
+
+
+
