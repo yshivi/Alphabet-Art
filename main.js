d1f00dbc7386c695959ad445e0dc20cc27fcd03c
Rajeev Ranjan Jha <rajeevmyname@gmail.com>
2023-12-18 00:50:53 +0530

    Refactor the code

--- main.js
+++ main.js
@@ -1,111 +1,131 @@
+//All words
+let words = ["Alligator","Bee","Cat",
+             "Dinosaur","Elephant","Fox",
+             "Giraffe","House","Igloo",
+             "Jellyfish","King","Ladybug",
+             "Mountains","Ninja","Octopus",
+             "Pirate","Queen","Rainbow",
+             "Strawberry", "Tree", "Unicorn",
+             "Volcano","Watermelon","Xylophone",
+             "Yo-yo","Zebra"]
+
+//Set up text to speech
+var msg = new SpeechSynthesisUtterance();
+var voices = window.speechSynthesis.getVoices();
+msg.voice = voices[7];
+
+// Add Alphabet Cards
 let container = document.querySelector(".container");
-let carouselImg = document.querySelector(".carouselImg");
+for(let i=65; i<=90; i++){
+    let alphabet = String.fromCharCode(i);
+
+    let card = document.createElement("div");
+    card.classList.add("card");
+    container.appendChild(card);
+
+    //backcard
+    let backCard = document.createElement("div");
+    let alphabetText = document.createElement("p");
+    alphabetText.innerText = alphabet;
+
+    backCard.classList.add("backCard");
+    backCard.appendChild(alphabetText);
+    card.appendChild(backCard);
+
+    //frontcard
+    let frontCard = document.createElement("div");
+    frontCard.classList.add("frontCard");
+    card.appendChild(frontCard);
+        //alphabet image
+    let imgContainer = document.createElement("div");
+    let alphabetImg = document.createElement("img");
+    alphabetImg.src = `https://placehold.co/312x256/pink/white?text=${alphabet}`;
+    imgContainer.appendChild(alphabetImg);
+    frontCard.appendChild(imgContainer);
+        //word for the alphabet
+    let word = document.createElement("p");
+    word.innerText = words[i - 65];
+    frontCard.appendChild(word);
+        //div containing icons at the bottom of each card
+    let faIcons = document.createElement("div");
+    faIcons.classList.add("fa-icons");
+    frontCard.appendChild(faIcons);
+        //Heart Icon           
+    let likeIcon = document.createElement("i");
+    likeIcon.classList.add("fa-regular", "fa-heart");
+    faIcons.appendChild(likeIcon);
+        //Flip Icon
+    let flipIcon = document.createElement("i");
+    flipIcon.classList.add("fa-solid", "fa-arrow-rotate-left");
+    faIcons.appendChild(flipIcon);
+        //Share Icon
+    let shareIcon = document.createElement("i");
+    shareIcon.classList.add("fa-solid", "fa-share-from-square");
+    faIcons.appendChild(shareIcon);
+    
+    //Show front card on click
+    card.addEventListener("click", () => {
+        card.classList.add("flipped");
+        msg.text = words[i-65];
+        window.speechSynthesis.speak(msg);
+    })
+
+    //Flip back on click
+    flipIcon.addEventListener("click",(event)=>{
+        card.classList.remove("flipped");
+        event.stopPropagation();
+    })
+
+    //Color the heart red when user clicks like
+    likeIcon.addEventListener("click",(event)=>{
+        likeIcon.classList.toggle("fa-solid");
+        likeIcon.style.color = "red";
+        event.stopPropagation();
+    })
+
+    //heart beat on hover
+    likeIcon.addEventListener("mouseover",(event)=>{
+        likeIcon.classList.add("fa-bounce");
+        event.stopPropagation();
+    })
+    likeIcon.addEventListener("mouseleave",(event)=>{
+        likeIcon.classList.remove("fa-bounce");
+        event.stopPropagation();
+    })
+
+}
+
+// Give design background to back of each alphabet card
+let allBackCards = document.querySelectorAll(".backCard");
+let patterns = ["pattern1", "pattern2", "pattern3",
+                "pattern4", "pattern5", "pattern6"];
+
+let j = 0;                
+allBackCards.forEach((backCard)=>{
+    if(j>5)
+        j=0;
+    backCard.classList.add(patterns[j]);
+    j++;
+})
+
+
+// Carousel
+let carouselImages = ["https://placehold.co/375x187/orange/white?text=Stickers","https://placehold.co/375x187/green/white?text=Workshop","https://placehold.co/375x187/purple/white?text=Contest"];
+let carouselImg = document.querySelector(".carousel img");
+let i = 0;
+
 let dots = document.querySelectorAll(".dot");
 
-let i = 0;    //This variable is for carousel    
-let alphabet; //This variable is used to create A to Z capital alphabets.
+setInterval(function(){
+    //move images every 4 seconds
+    i++;
+    if(i>2)
+        i=0;
+    carouselImg.src = carouselImages[i];
 
-const wordPro = [
-  "apple","ball","cat","dog","elephant",
-  "fish","goat","horse","igloo","joker",
-  "kite","lion","monkey","nose","octopus",
-  "pig","queen","rocket","snake","tiger",
-  "umbrella","van","watermellon","x-ray",
-  "yak","zebra",
-];
-const crslImg = [
-  "assets/carouselImages/IMAGE.png",
-  "assets/carouselImages/img2.jpeg",
-  "assets/carouselImages/img4.jpeg",
-];
-
-setInterval(function () {
-  i++;
-  let selectedDotId = "#dot" + i;
-  let selectedDot = document.querySelector(selectedDotId);
-  // console.log(selectedDot);
-  dots.forEach((dot) => (dot.style.backgroundColor = "white"));
-  selectedDot.style.backgroundColor = "black";
-  if (i >= 3) i = 0;
-  carouselImg.setAttribute("src", crslImg[i]);
-}, 2000);
-
-//this array for the background design of the front cards, in which we will put 6 different designs in the background of the front card with the help of loop.
-let design = ["design1", "design2", "design3", "design4", "design5", "design6"];
-
-//In this loop the i is running 65 to 90, which is creating 26 divs and j is working for the design array.
-for (let i = 65, k = 0, j = 0; i < 91; k++, i++, j++) {
-  if (j >= 6)
-    //This condition is telling that our j will run till 6, when the value of j exceeds 6 then the value of j will again become 0.
-    j = 0;
-
-  alphabet = String.fromCharCode(i);
-
-  //creating divs and img tags
-  let frontCard = document.createElement("div");
-  let backCard = document.createElement("div");
-  let cardsContainer = document.createElement("div");
-  let img = document.createElement("img");
-  let text = document.createElement("p");
-  let imgText = document.createElement("p");
-  let imgIconOne = document.createElement("img");
-  let imgIconTwo = document.createElement("img");
-  let imgIconThree = document.createElement("img");
-  let iconContainer = document.createElement("div");
-
-  //.........Giving icon src path to img tag.......
-  imgIconOne.src = "assets/backImgIcon/heartIcon.png";
-  imgIconTwo.src = "assets/backImgIcon/sizeIcon.png";
-  imgIconThree.src = "assets/backImgIcon/shareIcon.png";
-
-  //give class to div or tag we have created
-  cardsContainer.setAttribute("class", "cardsContainer");
-  backCard.setAttribute("class", "backCard");
-  frontCard.setAttribute("class", "frontCard");
-  cardsContainer.setAttribute("class", "cardsContainer");
-  img.setAttribute("class", "img");
-  text.setAttribute("class", "text");
-  imgText.setAttribute("class", "bckImgText");
-  imgIconOne.setAttribute("id", "imgIconOne");
-  imgIconTwo.setAttribute("id", "imgIconTwo");
-  imgIconThree.setAttribute("id", "imgIconThree");
-  iconContainer.setAttribute("class", "iconContainer");
-
-  frontCard.classList.add(design[j]);
-
-  //append all the divs and elements with the ones we have created for them.
-  container.appendChild(cardsContainer);
-  cardsContainer.appendChild(frontCard);
-  cardsContainer.appendChild(backCard);
-  backCard.appendChild(img);
-  frontCard.appendChild(text);
-  backCard.appendChild(iconContainer);
-  backCard.appendChild(imgText);
-  iconContainer.appendChild(imgIconOne);
-  iconContainer.appendChild(imgIconTwo);
-  iconContainer.appendChild(imgIconThree);
-  
-  imgText.innerText = wordPro[k];
-  text.innerText = alphabet;
-
-  img.setAttribute("src", `assets/${alphabet.toLowerCase()}.png`); //set image in back face of card, we write [i - 65] because in above loop we started i from 65 but our image position is starting from 0. So for changing i value 65 to 0 we write backgroundImage[i - 65]
-
-  // Flip on click
-  cardsContainer.addEventListener("click", () => {
-    cardsContainer.classList.toggle("flipped");
-    let msg = new SpeechSynthesisUtterance();
-    msg.text = wordPro[k];
-    window.speechSynthesis.speak(msg);
-  });
-
-  let heartClick = 0;
-  imgIconOne.addEventListener("click", () => {
-    if (heartClick === 0) {
-      imgIconOne.src = "assets/backImgIcon/redHeart.png";
-      heartClick = 1;
-    } else {
-      imgIconOne.src = "assets/backImgIcon/heartIcon.png";
-      heartClick = 0;
-    }
-  });
-}
+    //color dots corresponding to the image
+    let currentDotId = "#dot" + i;
+    let currentDot = document.querySelector(currentDotId);
+    dots.forEach((dot) => (dot.style.backgroundColor = "#59AFFF"));
+    currentDot.style.backgroundColor = "white";
+},4000)
