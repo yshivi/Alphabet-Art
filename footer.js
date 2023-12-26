8f69caa1c838b0e32b99b61880911c1f166b7598
Simran <simrantamang6008@gmail.com>
2023-12-26 15:42:44 +0530

    add some content in workshop page

---
+++ footer.js
@@ -0,0 +1,21 @@
+let footer = document.getElementById("footer")
+let footerContant = `<div class="social-media">
+<p>Follow Us</p>
+<ul>
+    <li><img src="../assets/social/fb.png" alt="Facebook"></li>
+    <li><img src="../assets/social/insta.png" alt="Instagram"></li>
+    <li><img src="../assets/social/you.png" alt="Youtube"></li>
+    <li><img src="../assets/social/in.png" alt="LinkedIn"></li>
+    <li><img src="../assets/social/x.png" alt="X"></li>
+</ul>
+</div>
+<div class="links">
+<ul>
+    <li>Contact Us</li>
+    <li>Terms Of Use</li>
+    <li>Report A Bug</li>
+    <li>Hire a Sapnewaali</li>
+    <li>Make a Donation</li>
+</ul>
+</div>`
+footer.innerHTML = footerContant;
