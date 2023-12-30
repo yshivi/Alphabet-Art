0e85f2ba4afb5f984a3b4a904e6ba0925d79bc48
Rajeev Ranjan Jha <rajeevmyname@gmail.com>
2023-12-30 11:17:27 +0530

    Change file address for github

--- header.js
+++ header.js
@@ -6,15 +6,15 @@ let nav =
 <!-- <NavigationMenu/> -->
 <nav>
     <div>
-        <a href="../stickers/index.html"><img src="../assets/navImages/sticker.png" alt="Stickers"></a>
+        <a href="../stickers/index.html"><img src="../Alphabet-Art/assets/navImages/sticker.png" alt="Stickers"></a>
         <p>Stickers</p>
     </div>
     <div>
-        <a href="../workshop/index.html"><img src="../assets/navImages/workshop.png" alt="Workshop"></a>
+        <a href="../workshop/index.html"><img src="../Alphabet-Art/assets/navImages/workshop.png" alt="Workshop"></a>
         <p>Workshop</p>
     </div>
     <div>
-        <a href="../contest/index.html"><img src="../assets/navImages/contest.png" alt="Contest"></a>
+        <a href="../contest/index.html"><img src="../Alphabet-Art/assets/navImages/contest.png" alt="Contest"></a>
         <p>Contest</p>
     </div>
 </nav>`
