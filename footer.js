63665fc95b2cfbbbced369a355e782d69bddcf82
shivi yadav <yshivi825@gmail.com>
2024-01-05 16:30:10 +0530

    except incoming changes

--- footer.js
+++ footer.js
@@ -1,15 +1,8 @@
 let footer = document.getElementById("footer")
 let footerContent = 
-`
-<div id="tag">
-    <a href="https://sajhesapne.org/">
-        <span>Made with <i class="fa-solid fa-heart" style="color: #ff8787;"></i> by Sapnewaalis!</span>
-    </a>
-</div>
-
-<div id="footer-links">
-    <div class="social-media">
-        <span>Follow Us</span>
+`<footer>
+<div class="social-media">
+    <p>Follow Us</p>
     <ul>
         <li><img src="../assets/social/fb.png" alt="Facebook"></li>
         <li><img src="../assets/social/insta.png" alt="Instagram"></li>
@@ -17,8 +10,8 @@ let footerContent =
         <li><img src="../assets/social/in.png" alt="LinkedIn"></li>
         <li><img src="../assets/social/x.png" alt="X"></li>
     </ul>
-    </div>
-    <div class="links">
+</div>
+<div class="links">
     <ul>
         <li>Contact Us</li>
         <li>Terms Of Use</li>
@@ -26,6 +19,6 @@ let footerContent =
         <li>Hire a Sapnewaali</li>
         <li>Make a Donation</li>
     </ul>
-    </div>
-</div>`
+</div>
+</footer>`
 footer.innerHTML = footerContent;
